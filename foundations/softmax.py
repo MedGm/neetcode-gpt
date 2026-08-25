b7ff871873864009ee2ad64967ea2962b22e1535
MOHAMED EL GORRIM <elgorrim.mohamed@etu.uae.ac.ma>
2026-08-25 20:01:59 +0100

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -3,23 +3,15 @@ from numpy.typing import NDArray
 
 
 class Solution:
-    
-    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        # z is a 1D NumPy array
-        # Formula: 1 / (1 + e^(-z))
-        # return np.round(your_answer, 5)
-        z_clipped = np.clip(z,-30,30)
-        
-        s = np.where(z>=0, 1 / (1 + np.exp(-z_clipped)), np.exp(z_clipped) / (1 + np.exp(z_clipped)))
-        return np.round(s, 5)
 
-    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        # z is a 1D NumPy array
-        # Formula: max(0, z) element-wise
-        relu = []
-        for z_elements in z:
-            if z_elements < 0:
-                relu.append(0.0)
-            else:
-                relu.append(z_elements)
-        return relu
+    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array of logits
+        # Hint: subtract max(z) for numerical stability before computing exp
+        # return np.round(your_answer, 4)
+        e = []
+
+        for i in z:
+            e.append(np.exp(i-max(z)))
+        
+        return np.round(e / sum(e), 4)
+        pass
